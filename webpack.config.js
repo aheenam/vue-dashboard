e75073043f563dd4ae1512d24328e7a7680a3adf
Rathes Sachchithananthan <rs@net-insiders.com>
2017-10-04 11:06:47 +0200

    Fixes package to work with npm

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+var inProduction = (process.env.NODE_ENV === 'production')
+
+module.exports = {
+    context: __dirname,
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'index.js'
+    },
+
+    externals: {
+        vue: 'vue',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loaders: ['vue-loader'],
+                exclude: /node_modules/,
+            },
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+
+    plugins: [
+
+        new webpack.LoaderOptionsPlugin({
+            minimize: inProduction
+        }),
+
+    ]
+
+};
+
+if (inProduction) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({ output: { comments: false } })
+    )
+}
